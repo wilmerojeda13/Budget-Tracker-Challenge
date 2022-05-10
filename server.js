fdc484f560ec830876cc18323e0bbfbd7b88ff01
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-05-09 21:22:17 -0400

    add updating server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 //Initialize app
 const app = express();
@@ -18,14 +17,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/budget', {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port 🌍 ${PORT}!`);
 });
